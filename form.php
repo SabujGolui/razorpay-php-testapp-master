d6146854f716c789bf182ce8d9c285d2df3a6083
Sabuj Golui <sabujgolui5#gmail.com>
2022-12-13 11:19:25 +0530

    added payment page

--- form.php
+++ form.php
@@ -1,60 +1,127 @@
 <!DOCTYPE html>
 <html lang="en">
+<!--divinectorweb.com-->
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="css/styles.css">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <title>Payments</title>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+  <link rel="stylesheet" href="/css/styles.css">
 </head>
 
-<body class="body">
-    <nav class="navbar navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="images/15.jpg" alt="LOGO" width="30" height="24" class="d-inline-block align-text-top">
-                Game Zone
-            </a>
-        </div>
-    </nav>
-    <div class="w-50 m-auto">
-        <form method="POST" action="pay.php">
-            <div class="form-group">
-                <label class="form-head" for="">User ID</label>
-                <input type="number" name="userId" class="form-control" id="userId" placeholder="eg.AD947209">
-            </div>
-            <div class="form-group">
-                <label class="form-head" for="">Select Recharge</label>
-                <select multiple class="form-control" name="amount" id="amount" >
-                    <option value="100">200 coins / Rs.100</option>
-                    <option value="200">500 coins / Rs.200</option>
-                    <option value="300">1000 coins / Rs.300</option>
-                    <option value="500">2000 coins / Rs.500</option>
-                    <option value="1000">5000 coins / Rs.1000</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label class="form-head" for="">Email address</label>
-                <input type="email" name="emailId" class="form-control" id="emailId" placeholder="name@example.com">
-            </div>
-            <div class="form-group">
-                <label class="form-head" for="">Phone Number</label>
-                <input type="number" name="phoneNo" class="form-control" id="phoneNo" placeholder="Mobile no.">
-            </div>
-            <br>
-            <button type="submit" id="pay" class="btn btn-primary">Pay Now</button>
-        </form>
-        <!-- <div class="buy-button mt-2">
-            <a href="#" class="btn btn-primary">Pay Now</a>
-        </div> -->
+<body>
+
+  <!-- =========================navbar================ -->
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/index.html">
+        <img src="/images/155.jpg" alt="Logo" width="30" class="d-inline-block align-text-top">
+        Game Zone
+      </a>
+      <form class="d-flex" role="search">
+        <button class="btn btn-outline-warning" type="submit">
+          Login
+        </button>
+      </form>
     </div>
+    </div>
+  </nav>
 
+  <div class="col-lg-12 col-md-12 " style="margin-top: 20px;">
+    <img src="/images/banner.jpg" class="img-fluid d-block mx-auto" alt="">
+  </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <div class="row justify-content-center " style="margin-top: 20px;">
+    <div class="col-12 text-center">
+      <div class="section-title mb-4 pb-2">
+        <h4 class="title mb-4">Games Zone</h4>
+        <p class="text-muted para-desc mx-auto mb-0">
+          Read and accept policy before using our site.
+          <span class="text-primary fw-bold">Email:</span> contact@gameskharido.top
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fill">
+    <div class="form-area">
+      <div class="container">
+        <div class="row single-form g-0">
+          <div class="col-sm-12 col-lg-6">
+            <div class="left">
+              <h3>
+                <span class="text-white mb-2 "">Select Recharge</span>
+                        </h3>
+                        <h5>
+                            <ul class=" list-group">
+                  <li class="list-group-item">200 coins / Rs. 100</li>
+                  <li class="list-group-item">500 coins / Rs. 200</li>
+                  <li class="list-group-item">1000 coins / Rs. 300</li>
+                  <li class="list-group-item">2000 coins / Rs. 500</li>
+                  <li class="list-group-item">5000 coins / Rs. 1000</li>
+                  </ul>
+                  </h5>
+            </div>
+          </div>
+          <div class="col-sm-12 col-lg-6">
+            <div class="right">
+              <i class="fa fa-caret-left"></i>
+              <form>
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">User ID</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">Phone Number</label>
+                  <textarea type="Number" class="form-control" id="phone"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Pay now</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <section class="mt-5">
+    <!-- Footer -->
+    <footer id="footer" class="text-center text-white" style="background-color: black;">
+      <!-- Grid container -->
+      <div class="container p-4 pb-0">
+        <!-- Section: CTA -->
+        <section class="">
+          <p class="d-flex justify-content-center align-items-center">
+            <span class="me-3">Register for free</span>
+            <button type="button" class="btn btn-outline-light btn-rounded">
+              Sign up!
+            </button>
+          </p>
+        </section>
+        <!-- Section: CTA -->
+      </div>
+      <!-- Grid container -->
+
+      <!-- Copyright -->
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2022 Copyright:
+        <a class="text-white" href="https://gamecoins.in">gamecoins.in</a>
+      </div>
+      <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+  </section>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
 </body>
 
 </html>
